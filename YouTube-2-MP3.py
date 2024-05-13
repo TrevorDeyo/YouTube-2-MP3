b922735164b0a1e3af97891fc358f299844961f2
Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-05-13 15:45:02 -0500

    Update YouTube-2-MP3.py

--- YouTube-2-MP3.py
+++ YouTube-2-MP3.py
@@ -1,29 +1,11 @@
-import os
-import yt_dlp
+from pytube import YouTube
 
-def download_youtube_playlist(playlist_url, output_directory):
-    # Create a yt_dlp object with options for downloading audio only in MP3
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': os.path.join(output_directory, '%(title)s.%(ext)s'),
-    }
+def download_ytvid_as_mp3():
+    video_url = input("Enter the URL of the YouTube video: ")
+    yt = YouTube(video_url)
+    video = yt.streams.filter(only_audio=True).first()
+    filename = f"{yt.title}.mp3"
+    video.download(filename=filename)
+    print(f"Download complete: {filename}")
 
-    # Download the playlist
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([playlist_url])
-
-def main():
-    # Specify the playlist URL and output directory
-    playlist_url = 'https://www.youtube.com/playlist?list=PL4waBsMLGZzK6gYLG2pd7RoyxD1TfD2uA'
-    output_directory = r'C:\Users\tdeyo\Desktop\Code\YouTube-2-MP3\mp3s'
-
-    # Download the YouTube playlist in MP3 format directly
-    download_youtube_playlist(playlist_url, output_directory)
-
-if __name__ == "__main__":
-    main()
+download_ytvid_as_mp3()
