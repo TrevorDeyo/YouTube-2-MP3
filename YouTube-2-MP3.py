516233bf311d295505c2ca680a0110e5a23b3e67
King Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-06-03 06:29:44 -0500

    Update YouTube-2-MP3.py

--- YouTube-2-MP3.py
+++ YouTube-2-MP3.py
@@ -3,6 +3,8 @@ from pytube import YouTube
 video_url = input("Enter the URL of the YouTube video: ")
 yt = YouTube(video_url)
 video = yt.streams.filter(only_audio=True).first()
-filename = f"{yt.title}.mp3"
+filename = f"{yt.title}.mp4"
 video.download(filename=filename)
 print(f"Download complete: {filename}")
+
+# so this is technically a mp4 with the video stripped I think?
